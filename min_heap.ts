d88e775bcdb3e052b1ba5d8a3854e5ac8e4005a1
ANBite <124651559+ANBite@users.noreply.github.com>
2024-09-27 23:01:49 -0600

    clase Ventas
    Se reorganizó la clase ventas, se agregaron //etiquetas para la mejor comprensión del código
    Se intentó colocar colores pero no se logró (seguiré intentando)

--- min_heap.ts
+++ min_heap.ts
@@ -1,91 +1,113 @@
 import { Node } from "./class_node";
 
-class Sales{  //clase ventas
-    private sale : Node[]; 
-    private items : number; 
+class Sales{  //clase Ventas
+    private sale : Node[]; //Array tipo node
+    private items : number; //Cantidad de elementos existentes
 
     constructor(size: number){ 
         this.sale = new Array(size + 1);
         this.items = 0;
     }
 
-    public isEmpty(): boolean{
-        return this.items == 0;
-    }
-
-    public seeMin(): string {
-        return "Producto: " + this.sale[1].getAction() + "Tiene un costo de: "+ this.sale[1].getPrice();
-    }
-
-    public getQuantity(): number{ 
-        return this.items;
-    }
-
-    public insert(value: Node): void{
-        if(this.items == this.sale.length - 1) 
-            this.resize(2 * this.sale.length);
-
-        this.items ++;
-        this.sale[this.items] = value; 
-        this.swap(this.items);
-    }
-
-    private swap(i : number): void{ 
-        let dad: number = Math.floor(i / 2)
-        while ( i > 1 && this.sale[dad].getPrice() > this.sale[i].getPrice()){ 
-            let temp: Node = this.sale[dad]; 
-            this.sale[dad] = this.sale[i]; 
-            this.sale[i] = temp; 
-            i = dad; 
-            dad = Math.floor(i / 2);
+    //Intercambio
+    private interchange(i : number): void{ 
+        //Se localiza al padre del valor ingresado
+        let temp_dad: number = Math.floor(i / 2)
+        //Mientras no estemos en la raíz y el valor del padre sea mayor al valor del dato ingresado
+        while ( i > 1 && this.sale[temp_dad].getPrice() > this.sale[i].getPrice()){ 
+            //Se guarda el padre en una variable momentánea
+            let momentary: Node = this.sale[temp_dad]; 
+            //El padre se intercambia con el dato ingresado:
+            this.sale[temp_dad] = this.sale[i]; 
+            this.sale[i] = momentary; 
+            //El padre subió y ahora corresponde operar ese valor
+            i = temp_dad; 
+            temp_dad = Math.floor(i / 2);
         }
     } 
 
+    //Reestructurar el árbol
+    private restructureTree(start: number): void {
+        //Mientras que el resultado de la multiplicación sea menor o igual a la cantidad de items existentes
+        while (2*start <= this.items){ 
+            //Se guarda en una variable temporal la posición 2 * start Y SE ASUME QUE ES EL HIJO IZQUIERDO
+            let temp_left_soon: number = 2 * start;
+            //Si el valor es menor a la cantidad de elementos existentes Y
+            //Si el valor de la posición del hijo izquierdo es mayor igual al valor de la posición hijo izquierdo + 1 (HIJO DERECHO)
+            if (temp_left_soon < this.items && this.sale[temp_left_soon].getPrice() >= this.sale[temp_left_soon + 1].getPrice()){
+                //Se cambia a hijo derecho
+                temp_left_soon++; 
+            }
+            //Si el valor de start es menor igual al valor del hijo izquierdo entonces se rompex
+            if (this.sale[start].getPrice() <= this.sale[temp_left_soon].getPrice()){
+                break;
+            }
+            //Hacemos intercambio burbuja entre los nodos para que el menor quede en la raíz
+            let temp_class : Node = this.sale[start];
+            this.sale[start] = this.sale[temp_left_soon];
+            this.sale[temp_left_soon] = temp_class;
+            //Verificamos si procede otro intercambio hacia abajo
+            start = temp_left_soon;
+        }
+    }
+
+    //Cambiar de tamaño
     private resize(newSize: number): void{
         let newSale: Node[] = new Array(newSize);
         for (let i = 1; i < this.sale.length; i++){
             newSale[i] = this.sale[i];
-        this.sale = newSale; //Se cambió la referencia Heap se perdió
+        this.sale = newSale; //Se cambió la referencia Sales se perdió
         }
     }
 
-    public getNextProduct(): string {
-        if(this.items == 0){
-            return "No hay productos disponibles";
+    //Insertar
+    public insert(value: Node): void{
+        //Verifica si la cantidad de items ingresados es igual a la capacidad del array
+        if(this.items == this.sale.length - 1) {
+            this.resize(2 * this.sale.length);
         }
-        let min : Node = this.sale[1]; 
-        this.sale[1] = this.sale[this.items]; 
-        delete this.sale[this.items]; 
-        this.items --;
-        this.sink(1);
-        return min.getActionAndPrice();
+
+        this.items ++;
+        //En la posición de items se agrega el nuevo valor
+        this.sale[this.items] = value; 
+        this.interchange(this.items);
     }
 
-    private sink(k: number): void {
-        while (2*k <= this.items){ 
-            let j: number = 2*k;
-            if (j < this.items && this.sale[j].getPrice() >= this.sale[j + 1].getPrice()){
-                j++; 
+
+    //Mostrar TODO
+    public showAll(): string {
+        let temp_count : number = this.items;
+        while (true) {
+            if(temp_count == 0){
+                return "No hay productos disponibles";
             }
-            if (this.sale[k].getPrice() <= this.sale[j].getPrice()){
-                break;
-            }
-            
-            let temp : Node = this.sale[k];
-            this.sale[k] = this.sale[j];
-            this.sale[j] = temp;
-
-            k = j;
+    
+            //Se obtiene el valor de la posición 1 (raíz)
+            let minimun : Node = this.sale[1];
+            //Se cambia el valor de la posición 1 con la posición útlima
+            this.sale[1] = this.sale[temp_count];
+            //se elimina el último elemento de la lista 
+            //delete this.sale[temp_count]; 
+            temp_count --;
+            //Se restructura el arbol otra vez, partiendo desde la raíz
+            this.restructureTree(1);
+            return minimun.getActionAndPrice();
         }
     }
 
+    //Verifica si está vacía el array
+    public isEmpty(): boolean{
+        return this.items == 0;
+    }
 
-    public print(): void{
-        let tree: string = ""; 
-        for (const value of this.sale){
-            tree += " " + value.getAction() + " ";
-        }
-        console.log(tree);
+    //Cantidad de elementos existentes
+    public getQuantity(): number{ 
+        return this.items;
+    }
+
+    //Mostrar la raíz
+    public seeMin(): string {
+        return "Producto: " + this.sale[1].getAction() + "Tiene un costo de: "+ this.sale[1].getPrice();
     }
 }
 
@@ -99,12 +121,14 @@ products.insert(new Node("Higos", 12));
 products.insert(new Node("Papas fritas", 5));
 
 
+
 let i: number = 0;
 let limit: number = products.getQuantity();
 
 while (i < limit){
-    console.log(products.getNextProduct());
+    console.log(products.showAll());
     i += 1;
 }
 
-console.log(products.getQuantity())
+
+
